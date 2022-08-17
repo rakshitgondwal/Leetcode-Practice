a300c0b13c96b1f3ea6c4f16310404fd45250d5d
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2022-08-17 20:03:26 +0530

    update 1 q

--- KthSymbolinGrammar.java
+++ KthSymbolinGrammar.java
@@ -1,19 +1,77 @@
+//See the tree below to see how values in each row relate to the parent row values. The rows and the indexes for the nodes in each row are 1-based.
+//
+//        Row1	        			  0
+//                               /       \
+//        Row2					0          1
+//                             /   \      /    \
+//        Row3				  0     1     1      0
+//                           / \    / \   / \   / \
+//        Row4				0  1   1   0  1  0  0  1
+//
+//        Index(for Row 4)->  1  2   3   4  5  6  7  8
+//        Notice that:
+//
+//        the parent of kth index in nth row is
+//        k/2 index in the n-1 row for even numbered indexes.
+//        (k+1)/2 index in the n-1 row for odd numbered indexes.
+//        Also notice that the kth value of the index in nth row is:
+//
+//        flipped (reversed) value of the parent i.e. k/2 index in the n-1 row for even numbered indexes.
+//        same value as for the parent i.e. the (k+1)/2 index in the n-1 row for odd numbered indexes.
+//        which gets us to this solution:
+
+
 public class KthSymbolinGrammar {
-    public static void main(String[] args){
-        System.out.println(kthGrammar(4,2));
+    public static void main(String[] args) {
+        System.out.println(kthGrammar(2,2));
     }
-    public static int kthGrammar(int n, int k){
-        if(n==1) {
+    public static int kthGrammar(int N, int K) {
+
+        // base case
+        if(N==1) {
             return 0;
         }
-        int parent = kthGrammar(n-1, (k+1)/2);
-        if(k%2 == 0) {
-            return parent == 1? 0 : 1;
+
+        if(K%2==1) {
+            return kthGrammar(N-1, (K+1)/2);
         } else {
-            return parent;
+            return flip(kthGrammar(N-1, K/2));
         }
     }
+
+    public static int flip(int val) {
+        return val==0? 1 : 0;
+    }
 }
+
+
+
+
+
+
+
+
+
+//The solution below is more optimized than the above one but the above one is more understandable.
+
+
+
+//public class KthSymbolinGrammar {
+//    public static void main(String[] args){
+//        System.out.println(kthGrammar(4,2));
+//    }
+//    public static int kthGrammar(int n, int k){
+//        if(n==1) {
+//            return 0;
+//        }
+//        int parent = kthGrammar(n-1, (k+1)/2);
+//        if(k%2 == 0) {
+//            return parent == 1? 0 : 1;
+//        } else {
+//            return parent;
+//        }
+//    }
+//}
 
 
 
