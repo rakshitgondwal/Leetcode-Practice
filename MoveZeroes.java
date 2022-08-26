874770f16533d66bcfde1a63c8fcecd8d14b6b05
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2022-08-26 15:31:17 +0530

    modify MoveZeroes.java

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -7,26 +7,36 @@ public class MoveZeroes {
         System.out.println(Arrays.toString(nums));
     }
     public static void moveZeroes(int [] nums){
-        for (int i = 0; i < nums.length; i++) {
-            if (i!=0){
-                if (nums[i-1]==0){
-                    swap(i-1,nums,i, nums.length-1-i);
-                }
+        int a = 0;
+        int b = a+1;
+
+        while(b< nums.length){
+            if (nums[a] == 0 && nums[b] !=0){
+                swap(a,b,nums);
+                a++;
+                b++;
+                continue;
             }
-            if (nums[i] == 0){
-                swap(i,nums,i+1, nums.length-1-i);
+            if (nums[a] == 0 && nums[b] ==0) {
+                b++;
+                continue;
+            }
+            if (nums[a] != 0 && nums[b] !=0) {
+                a++;
+                b++;
+                continue;
+            }
+            if (nums[a] != 0 && nums[b] ==0){
+                a++;
+                b++;
+                continue;
             }
         }
     }
-    public static void swap(int a,int[]nums,int b, int times){
-        while (times!=0){
+    public static void swap(int a,int b, int[]nums){
             int temp = nums[a];
             nums[a] = nums[b];
             nums[b] = temp;
-            a++;
-            b++;
-            times--;
         }
     }
 
-}
