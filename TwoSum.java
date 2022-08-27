859f516b612039a03893d55185f8f672f5bedf3a
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2022-08-27 21:35:00 +0530

    modified TwoSum

--- TwoSum.java
+++ TwoSum.java
@@ -2,23 +2,43 @@ import java.util.Arrays;
 
 public class TwoSum {
     public static void main(String[] args) {
-      int[] nums = {3,2,4};
-        int [] answer = twoSum(nums,6);
+        int[] nums = {2,5,5,11};
+        int[] answer = twoSum(nums, 10);
         System.out.println(Arrays.toString(answer));
     }
-    public static int[] twoSum(int[] nums, int target){
-        int [] ans = new int[2];
 
-        for (int i = 0; i < nums.length ; i++) {
-            for (int j = i+1; j < nums.length ; j++) {
+    public static int[] twoSum(int[]nums, int target) {
+        int[] ans = new int[2];
+        int i = 0;
+        int j = i+1;
 
-                if(nums[i]  == target - nums[j]){
-                    ans[0] = j;
-                    ans[1] = i;
-                    return ans;
-                 }
+        while (i< nums.length){
+            if (nums[i] + nums[j] == target){
+                ans[0] = i;
+                ans[1] = j;
+                return ans;
             }
-
-        } return null;
+            if (j== nums.length-1){
+                i++;
+                j=i+1;
+            }else j++;
+        }
+       return ans;
     }
 }
+//    public static int[] twoSum(int[] nums, int target){
+//        int [] ans = new int[2];
+//
+//        for (int i = 0; i < nums.length ; i++) {
+//            for (int j = i+1; j < nums.length ; j++) {
+//
+//                if(nums[i]  == target - nums[j]){
+//                    ans[0] = j;
+//                    ans[1] = i;
+//                    return ans;
+//                 }
+//            }
+//
+//        } return null;
+//    }
+//}
