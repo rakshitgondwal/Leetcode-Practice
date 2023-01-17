dbb4beb1602720492b557881ddb3fce49870ea03
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2023-01-17 19:01:29 +0530

    1 q add

---
+++ MergeTwoSortedLists.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode ans = new ListNode();
+        ListNode head = ans;
+        while(list1 != null && list2!= null){
+            if(list1.val < list2.val){
+                head.next = list1;
+                list1 = list1.next;
+                head = head.next;
+            }else{
+                head.next = list2;
+                list2 = list2.next;
+                head = head.next;
+            }
+        }
+        while(list1!=null){
+            head.next = list1;
+            list1 = list1.next;
+            head = head.next;
+        }
+        while(list2!=null){
+            head.next = list2;
+            list2 = list2.next;
+            head = head.next;
+        } return ans.next;
+    }
+}
