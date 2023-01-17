460ee47c65099ac720ba1ca12e435377f905e4f4
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2023-01-17 19:34:27 +0530

    1qadd

---
+++ LinkedListCycle.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null){return false;}
+        ListNode first = head;
+        ListNode second = head;
+
+        while(first != null && first.next != null){
+            first = first.next;
+            second = second.next.next;
+
+            if(first == second){
+                return true;
+            }
+        }
+        return false;
+    }
+}
